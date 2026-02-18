346db8da123b465888b95cbfb5662b6a93d5b10c
Its33shubh <shubhamkakloter123@gmail.com>
2026-02-18 19:24:04 +0530

    setting up public static files

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const path = require('path')
 const app= express()
 
 app.use(express.urlencoded({ extended:true}));
 app.use(express.json())
+app.use(express.static(path.join(__dirname,'public')));
 app.set('view engine', 'ejs')
 
 app.get('/', (req,res)=>{
